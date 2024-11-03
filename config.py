375c7039365f59b475d6f45f218fc29d1e1c50c5
Goku malik <168086405+Gokukhan1@users.noreply.github.com>
2024-11-03 23:40:32 +0530

    Update config.py

--- config.py
+++ config.py
@@ -9,5 +9,5 @@ API_HASH = getenv("API_HASH","05e5e695feb30e25bef47484cc006da7")
 BOT_TOKEN = getenv("BOT_TOKEN")
 OWNER_ID = int(getenv("OWNER_ID","7403621976"))
 
-MONGO_DB_URI = getenv("mongodb+srv://vivek:1234567890@cluster0.c48d8ih.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-MUST_JOIN = getenv("MUST_JOIN","WORLD_ALPHA")
+MONGO_DB_URI = getenv("mongodb+srv://Bikash:Bikash@bikash.yl2nhcy.mongodb.net/?retryWrites=true&w=majority")
+MUST_JOIN = getenv("MUST_JOIN","lolpagalokigc")
