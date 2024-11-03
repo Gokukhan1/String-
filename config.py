db538359ebd0e33aa3f425a26f8867ecf6c388c2
Goku malik <168086405+Gokukhan1@users.noreply.github.com>
2024-11-03 23:45:38 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ API_ID = int(getenv("API_ID","21803165"))
 API_HASH = getenv("API_HASH","05e5e695feb30e25bef47484cc006da7")
 
 BOT_TOKEN = getenv("BOT_TOKEN")
-OWNER_ID = int(getenv("OWNER_ID","7403621976"))
+OWNER_ID = int(getenv("OWNER_ID","7078181502"))
 
 MONGO_DB_URI = getenv("mongodb+srv://Bikash:Bikash@bikash.yl2nhcy.mongodb.net/?retryWrites=true&w=majority")
 MUST_JOIN = getenv("MUST_JOIN","lolpagalokigc")
