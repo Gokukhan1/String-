94cb51e86fb1feb582a5801b8c340a5d6bdd935b
Goku malik <168086405+Gokukhan1@users.noreply.github.com>
2024-11-26 12:34:09 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ load_dotenv()
 API_ID = int(getenv("API_ID","21803165"))
 API_HASH = getenv("API_HASH","05e5e695feb30e25bef47484cc006da7")
 
-BOT_TOKEN = "6836897719:AAGDajQiBIvoNyAW5SFL8r-uZRBS2V4-AdE"
+BOT_TOKEN = ""
 OWNER_ID = int(getenv("OWNER_ID","7078181502"))
 
 MONGO_DB_URI = getenv("mongodb+srv://Bikash:Bikash@bikash.yl2nhcy.mongodb.net/?retryWrites=true&w=majority")
